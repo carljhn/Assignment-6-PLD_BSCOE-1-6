4d47b322e299765ca21c632570fcad6d6799d058
carljhn <zoletacarljohn1@gmail.com>
2021-11-21 20:21:50 -1200

    Third commit: I added an else statement with a nested if-else statement

--- numSorter.py
+++ numSorter.py
@@ -24,3 +24,24 @@ if num1>=num2 and num1>=num3 and num1>num4:
                     print(num1, num4, num2, num3)
                 else: #num3>num2
                     print(num1, num4, num3, num2)
+
+else:
+    if num2>=num1 and num2>num3 and num2>=num4:
+        if num1>=num3 and num1>num4:
+            if num3>=num4:
+                print(num2, num1, num3, num4)
+            else: #num4>num3   
+                print(num2, num1, num4, num3)
+        else: #num3>num1
+            if num3>=num1 and num3>num4:
+                if num1>=num4:
+                    print(num2, num3, num1, num4)
+                else: #num4>num1
+                    print(num2, num3, num4, num1)
+
+            else:
+                if num4>=num3:
+                    if num3>=num1:
+                        print(num2, num4, num3, num1)
+                    else: #num1>num3
+                        print(num2, num4, num1, num3)
