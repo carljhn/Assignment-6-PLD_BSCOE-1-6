cd6f53c674b0cd9179fd0f468de48983b8b667f9
carljhn <zoletacarljohn1@gmail.com>
2021-11-21 14:28:33 -1200

    Initial commit: I have added the pseudocodes and the variables for the four numbers

---
+++ numSorter.py
@@ -0,0 +1,7 @@
+#Number Sorter
+#Create a program that ask 4 numbers. 
+#Print the 4 numbers from highest to lowest using only if-else statement.
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+num3=int(input("Enter third number: "))
+num4=int(input("Enter fourth number: "))
