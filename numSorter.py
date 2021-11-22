67fa0dff85ee852f57e365cd986d3b7af7f36f5d
carljhn <zoletacarljohn1@gmail.com>
2021-11-21 20:13:40 -1200

    Second commit: I added a single if statement with nested if-else statements

--- numSorter.py
+++ numSorter.py
@@ -5,3 +5,22 @@ num1=int(input("Enter first number: "))
 num2=int(input("Enter second number: "))
 num3=int(input("Enter third number: "))
 num4=int(input("Enter fourth number: "))
+
+if num1>=num2 and num1>=num3 and num1>num4:
+    if num2>=num3 and num2>num4:
+        if num3>=num4:
+            print(num1, num2, num3, num4)
+        else:
+            print(num1, num2, num4, num3)
+    else: 
+        if num3>=num2 and num3>num4:
+            if num2>=num4:
+                print(num1, num3, num2, num4)
+            else: #num4>num2
+                print(num1, num3, num4, num2)
+        else: 
+            if num4>num3:
+                if num2>num3:
+                    print(num1, num4, num2, num3)
+                else: #num3>num2
+                    print(num1, num4, num3, num2)
