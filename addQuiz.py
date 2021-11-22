a358a93dadc9070f88174d35e5711862980b8c32
carljhn <zoletacarljohn1@gmail.com>
2021-11-21 20:56:37 -1200

    Second commit: I utilized the import random and added the first question to solve

--- addQuiz.py
+++ addQuiz.py
@@ -3,3 +3,15 @@
 #Let the user answer and evaluate if the user has the correct answer
 #The program will ask the user 10 addition operation
 #Display the result summary of the 10 operations (ex 9/10)
+
+import random
+num1=random.randint(0, 99)
+num2=random.randint(0, 99)
+ans=int(input(f"Question 1. {num1}+{num2}="))
+total=num1+num2
+score=0
+if ans==total:
+    print("correct!")
+    score+=1
+else:
+    print("Incorrect, the right answer is", total)
