e6fd75ee997a42a1506e7128a3c06d9acd2defb9
carljhn <zoletacarljohn1@gmail.com>
2021-11-21 20:46:47 -1200

    Seventh commit: I nested another else statement within the previous commit

--- numSorter.py
+++ numSorter.py
@@ -69,3 +69,9 @@ else:
                         print(num4, num1, num2, num3)
                     else: #num3>=num2
                         print(num4, num1, num3, num2)
+                else:
+                    if num2>=num1 and num2>num3:
+                        if num1>=num3:
+                            print(num4, num2, num1, num3) 
+                        else: #num3>num1
+                            print(num4, num2, num3, num1)     
